63135eba91ba66828c62326b758b85c271669285
Wongrave <tomiatti1@hotmail.com>
2020-10-21 09:33:42 -0300

    adicao dos estilos das paginas home, landing e o css global

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Salve pessoal!</h1>
+        <h1>Salve SI2/1A68!</h1>
       </header>
     </div>
   );
