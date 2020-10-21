7a2e9e8f0d12dac592f266c92fe97d4a811e4a67
Wongrave <tomiatti1@hotmail.com>
2020-10-21 08:54:22 -0300

    limpando a aplicacao

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import QualquerNomeDeComponente from './hello-world'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Salve pessoal!</h1>
-        <QualquerNomeDeComponente nome="world" />
-        <QualquerNomeDeComponente nome="SI3 e SI4" />
-
       </header>
     </div>
   );
 }
-
-export default App;
--- src/hello-world.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function HelloWorld(props) {
-    return (
-        <h1> Hello {props.nome}! </h1>
-    );
-}
-
-export default HelloWorld;
