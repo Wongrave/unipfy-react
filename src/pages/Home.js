8a777b7dd7dca2f760aff657c5dd90ec0e1e2512
Wongrave <tomiatti1@hotmail.com>
2020-10-23 11:21:19 -0300

    criacao das divs de musica

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaPlay, FaHeart } from 'react-icons/fa'
+import { FaPlay, FaHeart, FaBackward, FaForward, FaVolumeUp, FaPlus } from 'react-icons/fa'
 
 import Sidebar from '../components/Sidebar'
 
@@ -16,7 +16,78 @@ export default function Home() {
                         Músicas recomendadas
                     </h1>
                     <div className="music">
-                        <img src="https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg" 
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+
+                    <h1>
+                        Minhas músicas
+                    </h1>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
                             alt="Linkin Park" className="music-image" />
                         <span className="music-name"> Numb </span>
                         <span className="music-artist"> Linkin Park </span>
@@ -27,12 +98,90 @@ export default function Home() {
                         </div>
                     </div>
                     <h1>
-                        Minhas músicas
+                        Amigos escutaram
                     </h1>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
                     <h1>
                         Amigos escutaram
                     </h1>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
+                    <div className="music">
+                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
+                            alt="Linkin Park" className="music-image" />
+                        <span className="music-name"> Numb </span>
+                        <span className="music-artist"> Linkin Park </span>
+                        <div className="music-buttons">
+                            <div className="music-duration"> 03:21 </div>
+                            <FaPlay size={14} />
+                            <FaHeart size={14} />
+                        </div>
+                    </div>
                 </div>
+                <div className="player">
+                    <div className="player-buttons">
+                        <FaBackward size={24} />
+                        <FaPlay size={24} />
+                        <FaForward size={24} />
+                        <FaVolumeUp size={24} />
+                        <div className="volume"> {'\u00A0'} </div>
+                    </div>
+                </div>
+            </div>
+            <div className="add-music">
+                <FaPlus size={32} />
             </div>
         </div>
     )
