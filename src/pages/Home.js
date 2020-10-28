325d67c3f64b3eb849ce1608c2e689e456b79ed5
Wongrave <tomiatti1@hotmail.com>
2020-10-28 11:21:23 -0300

    adicao das musicas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaPlay, FaHeart, FaBackward, FaForward, FaVolumeUp, FaPlus } from 'react-icons/fa'
 
 import Sidebar from '../components/Sidebar'
+import api from '../services/api'
 
 import "./styles/global.css"
 import "./styles/home.css"
 
 export default function Home() {
+    const [musicas, setMusicas] = useState([])
+    useEffect(() => {
+        api.get('musicas/returnAll').then(resposta => {
+            setMusicas(resposta.data)
+        })
+    }, [])
+
     return (
         <div id="home">
             <div className="page">
@@ -15,50 +23,24 @@ export default function Home() {
                     <h1>
                         Músicas recomendadas
                     </h1>
-                    <div className="music">
-                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
-                            alt="Linkin Park" className="music-image" />
-                        <span className="music-name"> Numb </span>
-                        <span className="music-artist"> Linkin Park </span>
-                        <div className="music-buttons">
-                            <div className="music-duration"> 03:21 </div>
-                            <FaPlay size={14} />
-                            <FaHeart size={14} />
-                        </div>
-                    </div>
-                    <div className="music">
-                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
-                            alt="Linkin Park" className="music-image" />
-                        <span className="music-name"> Numb </span>
-                        <span className="music-artist"> Linkin Park </span>
-                        <div className="music-buttons">
-                            <div className="music-duration"> 03:21 </div>
-                            <FaPlay size={14} />
-                            <FaHeart size={14} />
-                        </div>
-                    </div>
-                    <div className="music">
-                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
-                            alt="Linkin Park" className="music-image" />
-                        <span className="music-name"> Numb </span>
-                        <span className="music-artist"> Linkin Park </span>
-                        <div className="music-buttons">
-                            <div className="music-duration"> 03:21 </div>
-                            <FaPlay size={14} />
-                            <FaHeart size={14} />
-                        </div>
-                    </div>
-                    <div className="music">
-                        <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
-                            alt="Linkin Park" className="music-image" />
-                        <span className="music-name"> Numb </span>
-                        <span className="music-artist"> Linkin Park </span>
-                        <div className="music-buttons">
-                            <div className="music-duration"> 03:21 </div>
-                            <FaPlay size={14} />
-                            <FaHeart size={14} />
-                        </div>
-                    </div>
+                    {
+                        musicas.map(musica => {
+                            return (
+                                <div className="music" key={musica.id}>
+                                    <img src={musica.artista.imagem}
+                                        alt={musica.artista.nome} className="music-image" />
+                                    <span className="music-name"> {musica.nome} </span>
+                                    <span className="music-artist"> {musica.artista.nome} </span>
+                                    <div className="music-buttons">
+                                        <div className="music-duration"> {musica.duracao} </div>
+                                        <FaPlay size={14} />
+                                        <FaHeart size={14} />
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+
 
                     <h1>
                         Minhas músicas
@@ -85,7 +67,7 @@ export default function Home() {
                             <FaHeart size={14} />
                         </div>
                     </div>
-                    
+
                     <div className="music">
                         <img src='https://3.bp.blogspot.com/-ha49Q8OMvIg/VMowTN5HLVI/AAAAAAAAFjw/leOuwq00SvM/s1600/linkin-park1.jpg'
                             alt="Linkin Park" className="music-image" />
